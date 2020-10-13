0503836814350dad8b2b4e5a27ff160e967d526b
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-13 20:35:28 -0300

    Split printf

--- e03.cpp
+++ e03.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 
+const float galao = 0.26417;
+
 int main()
 {
 	SetConsoleOutputCP(1252);
@@ -23,7 +25,9 @@ int main()
 	printf("Quanto custa o galão americano de gasolina em Reais (R$)? ");
 	scanf("%f", &precoRealGalao);
 	
-	printf("\nPasseio: %d\nOff-road: %d\nLitros Passeio: %.1fl\nLitros Off-road: %.1fl\nPreço R$/galão: R$%.2f", passeio, offRoad, pLitros, oLitros, precoRealGalao);
+	printf("\nPasseio: %d\nOff-road: %d\n", passeio, offRoad);
+	printf("\nLitros Passeio: %.1fl\nLitros Off-road: %.1fl\n", pLitros, oLitros);
+	printf("\nPreço R$/galão: R$%.2f\nGalão: %.5f", precoRealGalao, galao);
 	
 	return 1;
 }
