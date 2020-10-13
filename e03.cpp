0c2f1e42412f1b2283897306fa13b8a4265e9703
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-13 20:30:21 -0300

    Read cost

--- e03.cpp
+++ e03.cpp
@@ -6,7 +6,7 @@ int main()
 	SetConsoleOutputCP(1252);
 	
 	int passeio = 0, offRoad = 0;
-	float pLitros = 0, oLitros = 0;
+	float pLitros = 0, oLitros = 0, precoRealGalao = 0;
 	
 	printf("Quantos veículos de passeio tem na empresa? ");
 	scanf("%d", &passeio);
@@ -14,13 +14,16 @@ int main()
 	printf("Quantos veículos off-road? ");
 	scanf("%d", &offRoad);
 	
-	printf("Quantos litros cabem num tanque de um veículo de passeio? ");
+	printf("Quantos litros cabem num tanque de veículo de passeio? ");
 	scanf("%f", &pLitros);
 	
-	printf("Quantos litros cabem num tanque de um veículo off-road? ");
+	printf("Quantos litros cabem num off-road? ");
 	scanf("%f", &oLitros);
 	
-	printf("\nPasseio: %d\nOff-road: %d\nLitros Passeio: %.1f\nLitros Off-road: %.1f", passeio, offRoad, pLitros, oLitros);
+	printf("Quanto custa o galão americano de gasolina em Reais (R$)? ");
+	scanf("%f", &precoRealGalao);
+	
+	printf("\nPasseio: %d\nOff-road: %d\nLitros Passeio: %.1fl\nLitros Off-road: %.1fl\nPreço R$/galão: R$%.2f", passeio, offRoad, pLitros, oLitros, precoRealGalao);
 	
 	return 1;
 }
