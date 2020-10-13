3cf6964b3b764ccabf5a82b0110f67d69f6d1acf
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-13 20:25:02 -0300

    Second data input

--- e03.cpp
+++ e03.cpp
@@ -6,6 +6,7 @@ int main()
 	SetConsoleOutputCP(1252);
 	
 	int passeio = 0, offRoad = 0;
+	float pLitros = 0, oLitros = 0;
 	
 	printf("Quantos veículos de passeio tem na empresa? ");
 	scanf("%d", &passeio);
@@ -13,7 +14,13 @@ int main()
 	printf("Quantos veículos off-road? ");
 	scanf("%d", &offRoad);
 	
-	printf("\nPasseio: %d\nOff-road: %d", passeio, offRoad);
+	printf("Quantos litros cabem num tanque de um veículo de passeio? ");
+	scanf("%f", &pLitros);
+	
+	printf("Quantos litros cabem num tanque de um veículo off-road? ");
+	scanf("%f", &oLitros);
+	
+	printf("\nPasseio: %d\nOff-road: %d\nLitros Passeio: %.1f\nLitros Off-road: %.1f", passeio, offRoad, pLitros, oLitros);
 	
 	return 1;
 }
