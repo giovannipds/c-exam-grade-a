6fbbd73a4eba46254c12950d8a08a24b7c9bd4cb
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-13 22:03:14 -0300

    Neperiano o/

--- e08.cpp
+++ e08.cpp
@@ -2,22 +2,32 @@
 #include <stdio.h>
 #include <windows.h>
 
-int fatorial(int n)
-{
-	return n <= 1 ? 1 : n * fatorial(n - 1);
-}
+int fatorial(int n);
+float numeroNeperiano(int n);
 
 int main()
 {
 	SetConsoleOutputCP(1252);
 	
-	int i = 5;
+	int n;
+	float e;
 	
-//	printf("Digite o preço do produto no estabelecimento 1: ");
-//	scanf("%f", &p1);
-
-	printf("Fatorial de %d: %d", i, fatorial(i));
+	printf("Insira o valor de N: ");
+	scanf("%d", &n);
+	
+	e = numeroNeperiano(n);
+	printf("Neperiano / valor aproximado de: %f\n", e);
 	
 	return 1;
+}
+
+int fatorial(int n)
+{
+	return n <= 1 ? 1 : n * fatorial(n - 1);
+}
+
+float numeroNeperiano(int n)
+{
+	return (float)1 / fatorial(n) + (n > 0 ? numeroNeperiano(n - 1) : 0);
 }
 
