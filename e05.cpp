dbbcb21f4de8103ac8db70bc06b4f36c39d00c7a
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-13 23:38:07 -0300

    Validate year/month and start validating day

--- e05.cpp
+++ e05.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <windows.h>
 
+const char *months[12] = {
+	"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+	"Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+};
+
+bool validYear(int year)
+{
+	return year >= 1900 && year <= 2100;
+}
+
+bool validMonth(int month)
+{
+	return month >= 1 && month <= 12;
+}
+
+bool validDay(int day, int month)
+{
+	const int monthDays[12] = {
+		31, 28, 31, 30, 31, 30,
+		31, 31, 30, 31, 30, 31
+	};
+	return day >= 1 && day <= monthDays[month - 1];
+}
+
+bool validDate(int day, int month, int year)
+{
+	return validYear(year) && validMonth(month) && validDay(day, month);
+}
+
 int main()
 {
 	SetConsoleOutputCP(1252);
@@ -16,7 +45,9 @@ int main()
 	printf("Insira o ano: ");
 	scanf("%d", &y);
 	
-	printf("Dia: %d \nMês: %d \nAno: %d", d, m, y);
+	printf("\nDia: %d \nMês: %d \nAno: %d \n\n", d, m, y);
+	
+	printf("Data %s.", validDate(d, m, y) ? "válida" : "inválida");
 	
 	return 1;
 }
