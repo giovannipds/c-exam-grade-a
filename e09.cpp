100620e68d6b87b0c290d630cc5d4215e3ec52d3
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-13 22:32:26 -0300

    Maybe solved 9

--- e09.cpp
+++ e09.cpp
@@ -9,14 +9,18 @@ int main()
 	
 	char str[8] = "2030678";
 	
-	printf("A soma dos dígitos é: ", sum(str));
+	printf("A soma dos dígitos é: %d", sum(str));
 	
 	return 1;
 }
 
 int sum(char* strPointer)
 {
-	printf("%s\n\n", strPointer);
-	return 0;
+	char c = *strPointer;
+	int n = *strPointer - 48;
+	
+//	printf("%c e %d\n", c, n);
+
+	return c != '\0' ? n + sum(++strPointer) : 0;
 }
 
