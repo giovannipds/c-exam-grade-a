97871f57aae968dc7fdca5d56d8a8c7423f310f9
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-14 00:01:23 -0300

    5 in progress

--- e05.cpp
+++ e05.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <windows.h>
 
 const char *months[12] = {
@@ -35,6 +36,24 @@ bool validDate(int day, int month, int year)
 	return validYear(year) && validMonth(month) && validDay(day, month, year);
 }
 
+char* longDate(int day, int month, int year)
+{
+	char date[80];
+	itoa(day, date, 10);
+	strcat(date, " de ");
+	strcat(date, months[month - 1]);
+	strcat(date, " de ");
+//	strcat(date, itoa(year, date, 10));
+//	strcpy(date, );
+	//%d de %s de %d. //d, months[m - 1], y
+	return strdup(date); //month + year;
+}
+
+int yearDate(int day, int month, int year)
+{
+	return 0;
+}
+
 int main()
 {
 	SetConsoleOutputCP(1252);
@@ -50,9 +69,13 @@ int main()
 	printf("Insira o ano: ");
 	scanf("%d", &y);
 	
-	printf("\nDia: %d \nMês: %d \nAno: %d \n\n", d, m, y);
+//	printf("\nDia: %d \nMês: %d \nAno: %d \n\n", d, m, y);
 	
-	printf("Data %s.", validDate(d, m, y) ? "válida" : "inválida");
+	printf("a) Data %s.\n", validDate(d, m, y) ? "válida" : "inválida");
+	
+	printf("b) Data por extenso: %s.\n", longDate(d, m, y));
+	
+	printf("c) Dia do ano: %d", yearDate(d, m, y));
 	
 	return 1;
 }
